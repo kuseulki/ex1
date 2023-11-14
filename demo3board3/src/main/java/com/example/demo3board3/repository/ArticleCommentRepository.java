9b19e49de73847653aa1502a78bdaebe8695948a
kuseulki <kuseulki@naver.com>
2023-11-14 20:02:10 +0900

    이전파일 삭제

--- demo3board3/src/main/java/com/example/demo3board3/repository/ArticleCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo3board3.repository;
-
-import com.example.demo3board3.domain.ArticleComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
-}
