3cf772f41a1e2149aff1bc073f75459047b170f1
kuseulki <kuseulki@naver.com>
2024-01-17 20:32:13 +0900

    #46 - 댓글 기능 수정

--- exboard5/src/main/java/com/a/exboard5/dto/request/ArticleCommentRequest.java
+++ exboard5/src/main/java/com/a/exboard5/dto/request/ArticleCommentRequest.java
@@ -5,15 +5,16 @@ import com.a.exboard5.dto.UserAccountDto;
 
 public record ArticleCommentRequest(Long articleId, String content) {
 
-    public static ArticleCommentRequest of(Long articleId, String content){
+    public static ArticleCommentRequest of(Long articleId, String content) {
         return new ArticleCommentRequest(articleId, content);
     }
 
-    public ArticleCommentDto toDto(UserAccountDto userAccountDto){
+    public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
         return ArticleCommentDto.of(
                 articleId,
                 userAccountDto,
                 content
         );
     }
+
 }
