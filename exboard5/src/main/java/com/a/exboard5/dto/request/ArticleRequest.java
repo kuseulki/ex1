f2dad7b1f7344df53db6de67705454da5e2b6285
kuseulki <kuseulki@naver.com>
2024-01-17 19:53:02 +0900

    #44 - 새로 추가된 부분. 1

---
+++ exboard5/src/main/java/com/a/exboard5/dto/request/ArticleRequest.java
@@ -0,0 +1,25 @@
+package com.a.exboard5.dto.request;
+
+import com.a.exboard5.dto.ArticleDto;
+import com.a.exboard5.dto.UserAccountDto;
+
+public record ArticleRequest(
+        String title,
+        String content,
+        String hashtag
+) {
+
+    public static ArticleRequest of(String title, String content, String hashtag) {
+        return new ArticleRequest(title, content, hashtag);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto) {
+        return ArticleDto.of(
+                userAccountDto,
+                title,
+                content,
+                hashtag
+        );
+    }
+
+}
