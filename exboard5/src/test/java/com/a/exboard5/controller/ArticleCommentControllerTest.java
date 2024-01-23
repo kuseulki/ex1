08d1278816ba27928a23fdb4335aabfef253cb35
kuseulki <kuseulki@naver.com>
2024-01-23 19:10:21 +0900

    #48 - 시큐리티 설정과 자바 대응 코드 구현
    스프링부트 2.7 시큐리티 설정 적용.
    인증 정보를 db로부터 부르는 빈과  패스워드 인코더 빈 까지 등록.
    영향 받는 부분은 컨트롤러와 서비스 코드에 반영.

--- exboard5/src/test/java/com/a/exboard5/controller/ArticleCommentControllerTest.java
+++ exboard5/src/test/java/com/a/exboard5/controller/ArticleCommentControllerTest.java
@@ -1,6 +1,7 @@
 package com.a.exboard5.controller;
 
 import com.a.exboard5.config.SecurityConfig;
+import com.a.exboard5.config.TestSecurityConfig;
 import com.a.exboard5.dto.ArticleCommentDto;
 import com.a.exboard5.dto.request.ArticleCommentRequest;
 import com.a.exboard5.service.ArticleCommentService;
@@ -12,6 +13,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Map;
@@ -25,14 +28,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
 @DisplayName("View 컨트롤러 - 댓글")
-@Import({SecurityConfig.class, FormDataEncoder.class})
+@Import({TestSecurityConfig.class, FormDataEncoder.class})
 @WebMvcTest(ArticleCommentController.class)
 class ArticleCommentControllerTest {
 
     private final MockMvc mvc;
     private final FormDataEncoder formDataEncoder;
 
-    @MockBean private ArticleCommentService articleCommentService;
+    @MockBean
+    private ArticleCommentService articleCommentService;
 
 
     public ArticleCommentControllerTest(@Autowired MockMvc mvc, @Autowired FormDataEncoder formDataEncoder) {
@@ -40,15 +44,15 @@ class ArticleCommentControllerTest {
         this.formDataEncoder = formDataEncoder;
     }
 
-
+    @WithUserDetails(value = "unoTest", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @DisplayName("[view][POST] 댓글 등록 - 정상 호출")
     @Test
     void givenArticleCommentInfo_whenRequesting_thenSavesNewArticleComment() throws Exception {
+
         // Given
         long articleId = 1L;
         ArticleCommentRequest request = ArticleCommentRequest.of(articleId, "test comment");
         willDoNothing().given(articleCommentService).saveArticleComment(any(ArticleCommentDto.class));
-
         // When & Then
         mvc.perform(
                         post("/comments/new")
@@ -62,25 +66,27 @@ class ArticleCommentControllerTest {
         then(articleCommentService).should().saveArticleComment(any(ArticleCommentDto.class));
     }
 
+    @WithUserDetails(value = "unoTest", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @DisplayName("[view][GET] 댓글 삭제 - 정상 호출")
     @Test
     void givenArticleCommentIdToDelete_whenRequesting_thenDeletesArticleComment() throws Exception {
         // Given
         long articleId = 1L;
         long articleCommentId = 1L;
-        willDoNothing().given(articleCommentService).deleteArticleComment(articleCommentId);
+        String userId = "unoTest";
+        willDoNothing().given(articleCommentService).deleteArticleComment(articleCommentId, userId);
 
         // When & Then
         mvc.perform(
-                        post("/comments/" + articleCommentId + "/delete")
-                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                                .content(formDataEncoder.encode(Map.of("articleId", articleId)))
-                                .with(csrf())
-                )
+                post("/comments/" + articleCommentId + "/delete")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(Map.of("articleId", articleId)))
+                        .with(csrf())
+        )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/articles/" + articleId))
                 .andExpect(redirectedUrl("/articles/" + articleId));
-        then(articleCommentService).should().deleteArticleComment(articleCommentId);
+        then(articleCommentService).should().deleteArticleComment(articleCommentId, userId);
     }
 
 }
