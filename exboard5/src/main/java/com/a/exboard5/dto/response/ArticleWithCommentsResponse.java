c416c3253c6573362144fdb42e1f547e011d8f66
kuseulki <kuseulki@naver.com>
2024-02-05 16:29:20 +0900

    #70 - 대댓글 비즈니스 로직 구현
    부모/자식 관계를 만들면서 entity -> dto 간 변환 로직, 댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법, 댓글과 대댓글을 db 데이터로부터 변환할 때 서로 계층 구조를 만들고 각각 정렬하는 과정을 구현

--- exboard5/src/main/java/com/a/exboard5/dto/response/ArticleWithCommentsResponse.java
+++ exboard5/src/main/java/com/a/exboard5/dto/response/ArticleWithCommentsResponse.java
@@ -1,12 +1,11 @@
 package com.a.exboard5.dto.response;
 
+import com.a.exboard5.dto.ArticleCommentDto;
 import com.a.exboard5.dto.ArticleWithCommentsDto;
 import com.a.exboard5.dto.HashtagDto;
-
-import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public record ArticleWithCommentsResponse(
@@ -20,16 +19,16 @@ public record ArticleWithCommentsResponse(
         String userId,
         Set<ArticleCommentResponse> articleCommentsResponse
 ) {
-
     public static ArticleWithCommentsResponse of(Long id, String title, String content, Set<String> hashtags, LocalDateTime createdAt, String email, String nickname, String userId, Set<ArticleCommentResponse> articleCommentResponses) {
         return new ArticleWithCommentsResponse(id, title, content, hashtags, createdAt, email, nickname, userId, articleCommentResponses);
     }
-
     public static ArticleWithCommentsResponse from(ArticleWithCommentsDto dto) {
+
         String nickname = dto.userAccountDto().nickname();
         if (nickname == null || nickname.isBlank()) {
             nickname = dto.userAccountDto().userId();
         }
+
         return new ArticleWithCommentsResponse(
                 dto.id(),
                 dto.title(),
@@ -42,9 +41,31 @@ public record ArticleWithCommentsResponse(
                 dto.userAccountDto().email(),
                 nickname,
                 dto.userAccountDto().userId(),
-                dto.articleCommentDtos().stream()
-                        .map(ArticleCommentResponse::from)
-                        .collect(Collectors.toCollection(LinkedHashSet::new))
+                organizeChildComments(dto.articleCommentDtos())
         );
+    }
+
+
+    private static Set<ArticleCommentResponse> organizeChildComments(Set<ArticleCommentDto> dtos) {
+        Map<Long, ArticleCommentResponse> map = dtos.stream()
+                .map(ArticleCommentResponse::from)
+                .collect(Collectors.toMap(ArticleCommentResponse::id, Function.identity()));
+
+        map.values().stream()
+                .filter(ArticleCommentResponse::hasParentComment)
+                .forEach(comment -> {
+                    ArticleCommentResponse parentComment = map.get(comment.parentCommentId());
+                    parentComment.childComments().add(comment);
+                });
+
+        return map.values().stream()
+                .filter(comment -> !comment.hasParentComment())
+                .collect(Collectors.toCollection(() ->
+                        new TreeSet<>(Comparator
+                                .comparing(ArticleCommentResponse::createdAt)
+                                .reversed()
+                                .thenComparingLong(ArticleCommentResponse::id)
+                        )
+                ));
     }
 }
